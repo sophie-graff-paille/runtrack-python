96b3ab82d8e4b84e9a0e065ad43ef1d98494a819
Sophie <115147269+sophie-graff-paille@users.noreply.github.com>
2023-01-03 10:33:30 +0100

    pythonJour1

---
+++ Jour01/Allerplusloin/ecaractère.py
@@ -0,0 +1,8 @@
+def finde():
+    chaîne=input("rentrer une chaîne de caractères : ")
+    if "e" in chaîne:
+        print("caractère présent")
+    else:
+        print("caractère introuvable")
+
+finde()
